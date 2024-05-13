151e2427bf1653ffef1da55d135b3ea03ef7cae9
proxza <proxza@gmail.com>
2024-05-13 19:38:45 +1000

    Updates

--- components/index.ts
+++ components/index.ts
@@ -2,5 +2,7 @@ import CustomButton from "./CustomButton";
 import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
 
-export { CustomButton, Footer, Hero, Navbar };
+export { CustomButton, Footer, Hero, Navbar, SearchBar, CustomFilter };
